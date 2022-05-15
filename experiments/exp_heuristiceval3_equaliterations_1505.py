f3ca053be888f63e94070e3e19b6934fa7bf81f1
maxja <max.jappert@gmail.com>
2022-05-15 19:31:15 +0200

    Added two new experiments

---
+++ experiments/exp_heuristiceval3_equaliterations_1505.py
@@ -0,0 +1,141 @@
+#! /usr/bin/env python
+
+"""
+Example experiment using a simple vertex cover solver.
+"""
+
+import glob
+import os
+import platform
+
+from downward.reports.absolute import AbsoluteReport
+from lab.environments import BaselSlurmEnvironment, LocalEnvironment
+from lab.experiment import Experiment
+from lab.reports import Attribute
+
+
+# Create custom report class with suitable info and error attributes.
+class BaseReport(AbsoluteReport):
+    INFO_ATTRIBUTES = ["time_limit", "memory_limit", "seed"]
+    ERROR_ATTRIBUTES = [
+        "domain",
+        "problem",
+        "algorithm",
+        "unexplained_errors",
+        "error",
+        "node",
+    ]
+
+
+NODE = platform.node()
+REMOTE = NODE.endswith(".scicore.unibas.ch") or NODE.endswith(".cluster.bc2.ch")
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+BENCHMARKS_DIR = os.path.join(SCRIPT_DIR, "benchmarks")
+
+### kann gelöscht werden
+# BHOSLIB_GRAPHS = sorted(glob.glob(os.path.join(BENCHMARKS_DIR, "bhoslib", "*.mis")))
+# RANDOM_GRAPHS = sorted(glob.glob(os.path.join(BENCHMARKS_DIR, "random", "*.txt")))
+# ALGORITHMS = ["2approx", "greedy"]
+# SEEDS = 2018
+####
+
+
+TIME_LIMIT = 6000
+MEMORY_LIMIT = 8192
+
+if REMOTE:
+    ENV = BaselSlurmEnvironment(email="max.jappert@unibas.ch")
+    #SUITE = BHOSLIB_GRAPHS + RANDOM_GRAPHS
+else:
+    ENV = LocalEnvironment(processes=8)
+    # Use smaller suite for local tests.
+    #SUITE = BHOSLIB_GRAPHS[:1] + RANDOM_GRAPHS[:1]
+
+
+### muss angepasst werden
+ATTRIBUTES = [
+    "p1_points",
+    "p2_points",
+    "p1_contemplation_time",
+    "p2_contemplation_time",
+    "points_difference"
+    #Attribute("solved", absolute=True),
+]
+####
+
+
+# Create a new experiment.
+exp = Experiment(environment=ENV)
+exp.add_resource("solver", os.path.join(SCRIPT_DIR, "carcassonne.jar"))
+# Add custom parser.
+exp.add_parser("parser.py")
+
+ALGORITHMS = dict()
+
+# In this experiment we're testing if we can beat a player with a heuristic playout while still being
+# more efficient.
+
+tree_policies = dict()
+tree_policies.update({'boltzmann': 7})
+tree_policies.update({'uct': 7})
+tree_policies.update({'uct-tuned': 13})
+tree_policies.update({'epsilon-greedy': 0.3})
+tree_policies.update({'decaying-epsilon-greedy': 1})
+tree_policies.update({'heuristic-mcts': 0})
+
+for type in tree_policies.keys():
+    key1 = f'{type}1'
+    key2 = f'{type}2'
+
+    value1 = ['--p1',  f'{type}', '--p2', f'{type}', '--p1trainingiterations', '200', '--p2trainingiterations', '200', '--p1playout', 'heuristic', '--p1explorationterm', f'{tree_policies[type]}', '--p2explorationterm', f'{tree_policies[type]}']
+    value2 = ['--p1',  f'{type}', '--p2', f'{type}', '--p1trainingiterations', '200', '--p2trainingiterations', '200', '--p2playout', 'heuristic', '--p1explorationterm', f'{tree_policies[type]}', '--p2explorationterm', f'{tree_policies[type]}']
+
+    ALGORITHMS.update({key1: value1})
+    ALGORITHMS.update({key2: value2})
+
+for algo_name, algo_cmd in ALGORITHMS.items():
+    for seed in range(5):
+        # loop over both positionings of players?
+        run = exp.add_run()
+
+        algo_cmd_string = ''
+
+        for arg in algo_cmd:
+            algo_cmd_string = algo_cmd_string + arg + ' '
+
+        run.add_command(
+            f"{algo_name}",
+            ["java",  "-jar", "{solver}", algo_cmd, "--deckseed", seed, "-v", "false"],
+            time_limit=TIME_LIMIT,
+            memory_limit=MEMORY_LIMIT,
+        )
+        # AbsoluteReport needs the following attributes:
+        # 'domain', 'problem' and 'algorithm'.
+        domain = "carcassonne"
+        task_name = algo_name
+        run.set_property("problem", task_name)
+        run.set_property("domain", domain)
+        run.set_property("algorithm", algo_name)
+        # BaseReport needs the following properties:
+        # 'time_limit', 'memory_limit', 'seed'.
+        run.set_property("time_limit", TIME_LIMIT)
+        run.set_property("memory_limit", MEMORY_LIMIT)
+        run.set_property("seed", seed)
+        # Every run has to have a unique id in the form of a list.
+        run.set_property("id", [domain, algo_name, f"{seed}"])
+
+# Add step that writes experiment files to disk.
+exp.add_step("build", exp.build)
+
+# Add step that executes all runs.
+exp.add_step("start", exp.start_runs)
+
+# Add step that collects properties from run directories and
+# writes them to *-eval/properties.
+exp.add_fetcher(name="fetch")
+
+# Make a report.
+exp.add_report(BaseReport(attributes=ATTRIBUTES), outfile="report.html")
+
+# Parse the commandline and run the given steps.
+exp.run_steps()
