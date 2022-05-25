9114210206e7a9a5b4da4e4fd5c9e17ebaf0e6b8
maxja <max.jappert@gmail.com>
2022-05-25 17:20:30 +0200

    Added new experiment

--- experiments/exp_playouts4.py
+++ experiments/exp_playouts4.py
@@ -88,8 +88,8 @@ for tree_policy in tree_policies.keys():
     key1 = f'{tree_policy}-1'
     key2 = f'{tree_policy}-2'
 
-    value1 = ['--p1',  f'{tree_policy}', '--p2',  f'{tree_policy}', '--p1trainingiterations', '2000', '--p2trainingiterations', '500', '--p1explorationterm', f'{tree_policies[tree_policy]}', '--p2explorationterm', f'{tree_policies[tree_policy]}', '--p1playout', 'direct-heuristic']
-    value2 = ['--p1',  f'{tree_policy}', '--p2',  f'{tree_policy}', '--p1trainingiterations', '500', '--p2trainingiterations', '2000', '--p1explorationterm', f'{tree_policies[tree_policy]}', '--p2explorationterm', f'{tree_policies[tree_policy]}', '--p2playout', 'direct-heuristic']
+    value1 = ['--p1',  f'{tree_policy}', '--p2',  f'{tree_policy}', '--p1trainingiterations', '3500', '--p2trainingiterations', '500', '--p1explorationterm', f'{tree_policies[tree_policy]}', '--p2explorationterm', f'{tree_policies[tree_policy]}', '--p1playout', 'direct-heuristic']
+    value2 = ['--p1',  f'{tree_policy}', '--p2',  f'{tree_policy}', '--p1trainingiterations', '500', '--p2trainingiterations', '3500', '--p1explorationterm', f'{tree_policies[tree_policy]}', '--p2explorationterm', f'{tree_policies[tree_policy]}', '--p2playout', 'direct-heuristic']
 
     ALGORITHMS.update({key1: value1})
     ALGORITHMS.update({key2: value2})
